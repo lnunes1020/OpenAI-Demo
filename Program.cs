4d42ee03fbcdce87883b5dd398f553c87870378f
ln514476 <ln514476@gsk.com>
2024-12-31 13:42:26 -0500

    generates image of kitten

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+using OpenAI_API;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var api=new OpenAIAPI("Your API Key");
+var request = new OpenAI_API.Images.ImageGenerationRequest
+{
+    Prompt = "A cute kitten in a basket"
+};
+
+var response = api.ImageGenerations.CreateImageAsync(request).Result;
+Console.WriteLine("Image URL: {0}", response.Data[0].Url);
